28026b908415d1b262556310b5d22893095ae897
Kenny Kato <60533850+ktkato@users.noreply.github.com>
2020-02-07 13:47:14 -0600

    Update datamining-ex01-knn.R

--- datamining-ex01-knn.R
+++ datamining-ex01-knn.R
@@ -1,14 +1,17 @@
 library(tidyverse)
 library(FNN)
+library(foreach)
 
-# omit unnecessary columns
+sclass <- read.csv("~/sclass.csv")
+
+# Omitting unnecessary columns
 
 keep = c("trim", "price", "mileage")
 sclass = sclass[keep]
 
 summary(sclass)
 
-# defining two datasets
+# Defining two datasets
 
 sclass350 = subset(sclass, trim == '350')
 sclass65amg = subset(sclass, trim == '65 AMG')
@@ -16,9 +19,9 @@ sclass65amg = subset(sclass, trim == '65 AMG')
 #####
 
 
-# split data into training and testing set
+# Splitting data into training and testing set
 
-  ## defining set sizes
+  ## Defining set sizes
 
 N350 = nrow(sclass350)
 Ntrain350 = floor(0.8*N350)
@@ -28,19 +31,19 @@ N65 = nrow(sclass65amg)
 Ntrain65 = floor(0.8*N65)
 Ntest65 = N65 - Ntrain65
 
-  ## random sample for training set
+  ## Random sample for training set
 
 trainind_350 = sample.int(N350, Ntrain350, replace = FALSE)
 trainind_65AMG = sample.int(N65, Ntrain65, replace = FALSE)
 
-  ## define training and testing sets
+  ## Defining training and testing sets
 Dtrain_350 = sclass350[trainind_350,]
 Dtest_350 = sclass350[-trainind_350,]
 
 Dtrain_65AMG = sclass65amg[trainind_65AMG,]
 Dtest_65AMG = sclass65amg[-trainind_65AMG,]
 
-  ## reorder rows by mileage
+  ## Reordering rows by mileage
 Dtest_350 = arrange(Dtest_350, mileage)
 head(Dtest_350)
 
@@ -58,11 +61,12 @@ Y_train_65AMG = select(Dtrain_65AMG, price)
 X_test_65AMG = select(Dtest_65AMG, mileage)
 Y_test_65AMG = select(Dtest_65AMG, price)
 
+
 ######
 
 
-# run KNN starting at K = 2 and going as high as needed
-# for each value of K, fit model to training set and make predictions on test set
+# Running KNN starting at K = 2 and going as high as needed
+# Fitting model to training set and make predictions on test set
 
   ## Predicting values with KNN, sclass 350
 
@@ -84,37 +88,23 @@ ypred_knn332_350 = knn332_350$pred
 
 knn3_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=3)
 knn10_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=10)
-knn20_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=20)
 knn30_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=30)
-knn40_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=40)
-knn50_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=50)
 knn75_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=75)
 knn100_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=100)
-knn125_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=125)
-knn150_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=150)
-knn175_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=175)
-knn200_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=200)
 knn233_65 = knn.reg(train = X_train_65AMG, test = X_test_65AMG, y = Y_train_65AMG, k=233)
 
 ypred_knn3_65 = knn3_65$pred
 ypred_knn10_65 = knn10_65$pred
-ypred_knn20_65 = knn20_65$pred
 ypred_knn30_65 = knn30_65$pred
-ypred_knn40_65 = knn40_65$pred
-ypred_knn50_65 = knn50_65$pred
 ypred_knn75_65 = knn75_65$pred
 ypred_knn100_65 = knn100_65$pred
-ypred_knn125_65 = knn125_65$pred
-ypred_knn150_65 = knn150_65$pred
-ypred_knn175_65 = knn175_65$pred
-ypred_knn200_65 = knn200_65$pred
 ypred_knn233_65 = knn233_65$pred
 
 
 ######
 
 
-# calculate out-of-sample RMSE for each value of K
+# Calculating out-of-sample RMSE for each value of K
   
   ## Defining RMSE function
 
@@ -131,14 +121,69 @@ rmse(Y_test_350, ypred_knn332_350)
 
 rmse(Y_test_65AMG, ypred_knn3_65)
 rmse(Y_test_65AMG, ypred_knn10_65)
-rmse(Y_test_65AMG, ypred_knn20_65)
 rmse(Y_test_65AMG, ypred_knn30_65)
-rmse(Y_test_65AMG, ypred_knn40_65)
-rmse(Y_test_65AMG, ypred_knn50_65)
 rmse(Y_test_65AMG, ypred_knn75_65)
 rmse(Y_test_65AMG, ypred_knn100_65)
-rmse(Y_test_65AMG, ypred_knn125_65)
-rmse(Y_test_65AMG, ypred_knn150_65)
-rmse(Y_test_65AMG, ypred_knn175_65)
-rmse(Y_test_65AMG, ypred_knn200_65)
 rmse(Y_test_65AMG, ypred_knn233_65)
+
+
+######
+
+# Plotting RMSE vs. K
+
+  ## 350
+
+k_grid350 = seq(from = 3, to = Ntrain350)
+rmse_grid350 = foreach(k = k_grid350, .combine='c') %do% {
+  knn350 = knn.reg(X_train_350, X_test_350, Y_train_350, k = k)
+  rmse(Y_test_350, knn350$pred)
+}
+
+rmse_grid350 = data.frame(K = k_grid350, RMSE = rmse_grid350)
+
+ggplot(data = rmse_grid350) +
+  geom_path(mapping = aes(x = K, y = RMSE)) +
+  ggtitle("RMSE vs. K: S Class 350")
+
+  ## 65 AMG
+
+k_grid65 = seq(from = 3, to = Ntrain65)
+rmse_grid65 = foreach(k = k_grid65, .combine='c') %do% {
+  knn65 = knn.reg(X_train_65AMG, X_test_65AMG, Y_train_65AMG, k = k)
+  rmse(Y_test_65AMG, knn65$pred)
+}
+
+rmse_grid65 = data.frame(K = k_grid65, RMSE = rmse_grid65)
+
+ggplot(data = rmse_grid65) +
+  geom_path(mapping = aes(x = K, y = RMSE)) +
+  ggtitle("RMSE vs. K: S Class 65 AMG")
+
+
+# Plotting fitted models at optimal K
+
+  ## 350
+
+optimal_k350 = rmse_grid350$K[which.min(rmse_grid350$RMSE)]
+knn_optimal350 = knn.reg(X_train_350, X_test_350, Y_train_350, optimal_k350)
+optimal_pred350 = knn_optimal350$pred
+optimal_rmse350 = rmse(Y_test_350, optimal_pred350)
+Dtest_350$pred = optimal_pred350
+
+scatter350 = ggplot(data = sclass350) +
+  geom_point(mapping = aes(x = mileage, y = price))
+scatter350 + geom_path(data = Dtest_350, mapping = aes(x = mileage, y = pred), color = 'blue') +
+  ggtitle("S Class 350 with Fitted Line")
+
+  ## 65 AMG
+
+optimal_k65 = rmse_grid65$K[which.min(rmse_grid65$RMSE)]
+knn_optimal65 = knn.reg(X_train_65AMG, X_test_65AMG, Y_train_65AMG, optimal_k65)
+optimal_pred65 = knn_optimal65$pred
+optimal_rmse65 = rmse(Y_test_65AMG, optimal_pred65)
+Dtest_65AMG$pred = optimal_pred65
+
+scatter65 = ggplot(data = sclass65amg) +
+  geom_point(mapping = aes(x = mileage, y = price))
+scatter65 + geom_path(data = Dtest_65AMG, mapping = aes(x = mileage, y = pred), color = 'red') +
+  ggtitle("S Class 65 AMG with Fitted Line")
